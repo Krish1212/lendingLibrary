bb89d7dd7943bae07663c4be2c981ddd52698a16
Krishnamoorthy <mailkimu@gmail.com>
2018-04-13 12:55:11 +0530

    Testing and testing again

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,7 +19,7 @@ export class HomePage {
     pages:''
   };
   selectedFiles: FileList;
-  currentUpload = new Upload();
+  currentUpload = new Upload;
   loading:Loading;
   
   constructor(public navCtrl: NavController, 
