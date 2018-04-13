037160629a448c7867f9b46e9b30dfcd7b95e3b2
Krishnamoorthy <mailkimu@gmail.com>
2018-04-13 12:41:33 +0530

    Reverting the date formating to original code

--- src/app/models/upload.ts
+++ src/app/models/upload.ts
@@ -5,7 +5,7 @@ export class Upload {
     name:string;
     url:string;
     progress:number;
-    createdAt: Date = new Date().toISOString();
+    createdAt: Date = new Date();
   
     constructor(file:File) {
       this.file = file;
